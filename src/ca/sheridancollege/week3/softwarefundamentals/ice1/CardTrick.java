c9344d6975a41f8eab74ba31ba234bb735a51d29
KarandeepO7 <106174522+KarandeepO7@users.noreply.github.com>
2022-05-24 16:02:33 -0400

    Third Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,4 +1,5 @@
-]package pickacard;
+package pickacard;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random; 
 /**
@@ -6,19 +7,18 @@ import java.util.Random;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Karandeep
- Date-24-05-2022
+  Date-24-05-2022
  */
 public class CardTrick {
     
 public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-
         Random ran = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-
+            
             //c.setValue(insert call to random number generator here)
             c.setValue(ran.nextInt(13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
@@ -28,10 +28,25 @@ public static void main(String[] args)
 
         //insert code to ask the user for Card value and suit, create their card
 
+        //Creating default card for the user
+        Card user = new Card();
+        int value;
+        String suit;
+        boolean found = false;
+        Scanner search = new Scanner(System.in);
+
+        System.out.println("PLEASE ENTER THE VALUE OF CARD FROM 1 TO 13:  ");
+        value = search.nextInt();
+        System.out.println("PLEASE ENTER THE SUIT OF THE CARD" + Arrays.toString(Card.SUITS));
+        suit = search.next();
+
+        //setting the value and the suit for the user card
+        user.setValue(value);
+        user.getSuit();
         // and search magicHand here
 
         //Then report the result here
-
+        
     }
-
-} 
+    
+}
