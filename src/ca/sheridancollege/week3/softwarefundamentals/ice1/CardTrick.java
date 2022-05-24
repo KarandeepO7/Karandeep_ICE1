68c1677baed123b133cdd97c4066f332a21539d7
KarandeepO7 <106174522+KarandeepO7@users.noreply.github.com>
2022-05-24 16:04:09 -0400

    Added Code to ask user to pick any card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Karandeep
-  Date-24-05-2022
+ Date-24-05-2022
  */
 public class CardTrick {
     
@@ -25,28 +25,40 @@ public static void main(String[] args)
             c.setSuit(Card.SUITS[ran.nextInt(4)]);
             magicHand[i] = c;
         }
-
+        
         //insert code to ask the user for Card value and suit, create their card
-
+        
         //Creating default card for the user
         Card user = new Card();
         int value;
         String suit;
         boolean found = false;
         Scanner search = new Scanner(System.in);
-
+        
         System.out.println("PLEASE ENTER THE VALUE OF CARD FROM 1 TO 13:  ");
         value = search.nextInt();
         System.out.println("PLEASE ENTER THE SUIT OF THE CARD" + Arrays.toString(Card.SUITS));
         suit = search.next();
-
+        
         //setting the value and the suit for the user card
         user.setValue(value);
         user.getSuit();
         // and search magicHand here
 
         //Then report the result here
-        
+        for(int i=0; i<magicHand.length; i++){
+            if(magicHand[i].getValue() == user.getValue() && magicHand[i].getSuit().equalsIgnoreCase(user.getSuit())){
+
+                found =true;
+                break;
+            }
+         }
+
+        //Then report the result here
+        if(found)
+            System.out.println("USER'S CARD IS IN THE MAGIC HAND OF RANDOM CARDS");
+        else
+            System.out.println("USER'S CARD IS NOT IN THE MAGIC HAND OF RANDOM CARDS");
     }
-    
-}
+
+} 
