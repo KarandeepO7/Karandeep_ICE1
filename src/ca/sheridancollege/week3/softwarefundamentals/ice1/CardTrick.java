427a3ca6948d409821cdee02d8935cd515981290
KarandeepO7 <106174522+KarandeepO7@users.noreply.github.com>
2022-05-24 16:01:05 -0400

    Made code to generate the random number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,32 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+]package pickacard;
+import java.util.Scanner;
+import java.util.Random; 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author ----
+ * @author Karandeep
+ Date-24-05-2022
  */
 public class CardTrick {
     
-    public static void main(String[] args)
+public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+
+        Random ran = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+
             //c.setValue(insert call to random number generator here)
+            c.setValue(ran.nextInt(13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[ran.nextInt(4)]);
+            magicHand[i] = c;
         }
-        
+
         //insert code to ask the user for Card value and suit, create their card
+
         // and search magicHand here
+
         //Then report the result here
+
     }
-    
-}
+
+} 
