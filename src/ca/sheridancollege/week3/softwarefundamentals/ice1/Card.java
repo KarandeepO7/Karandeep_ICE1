531f99fdf0f83670ccd757bcefcd882ee1dd974a
HP <HP@10.0.0.125>
2022-05-24 15:39:21 -0400

    First commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,53 +1,39 @@
+package pickacard;
+import java.util.Scanner;
+import java.util.Random; 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+ * Name: Karandeep
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author ____
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author Karandeep
+    Date- 24-5-2022
  */
-public class Card {
-
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
-
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
-
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
+public class CardTrick {
     
-}
+public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+
+        Random ran = new Random();
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+
+            //c.setValue(insert call to random number generator here)
+            c.setValue(ran.nextInt(13)+1);
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[ran.nextInt(4)]);
+            magicHand[i] = c;
+        }
+
+        //insert code to ask the user for Card value and suit, create their card
+
+        // and search magicHand here
+
+        //Then report the result here
+
+    }
+
+} 
